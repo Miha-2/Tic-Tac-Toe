033f56ed19afcd6179342d8409bd0531b712a158
Miha <52750967+Miha-2@users.noreply.github.com>
2020-11-15 21:31:27 +0100

    Feature, bugfix
    -fixed win-pointer bug
    -player name is localy saved -> QoL
    -now allowing both landscape orientations

--- Assets/StartCanvas.cs
+++ Assets/StartCanvas.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -10,11 +11,21 @@ public class StartCanvas : MonoBehaviour
     [SerializeField] private TMP_InputField _inputText = null;
     [SerializeField] private GameObject _infoText = null;
     private NetworkManager _netManager;
-    
+    private const string Nickname = "Nickname";
+
     public void SetReference(NetworkManager netManager)
     {
         _netManager = netManager;
     }
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey(Nickname))
+        {
+            _inputText.text = PlayerPrefs.GetString(Nickname);
+            _goOnlineBtn.interactable = true;
+        }
+    }
+    
     public void OnUpdatedName()
     {
         _goOnlineBtn.interactable = !string.IsNullOrEmpty(_inputText.text);
@@ -25,6 +36,7 @@ public class StartCanvas : MonoBehaviour
         _goOnlineBtn.gameObject.SetActive(false);
         _inputText.gameObject.SetActive(false);
         _infoText.SetActive(true);
+        PlayerPrefs.SetString(Nickname, _inputText.text);
         _netManager.Connect(_inputText.text);
     }
 }
